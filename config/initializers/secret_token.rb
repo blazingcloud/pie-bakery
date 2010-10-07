20105d6e638c0241d419bede3af3263b226bb9d8
Sarah Allen <sarah@ultrasaurus.com>
2010-10-07 11:35:51 -0700

    new rails app 1.9.2@rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bakery::Application.config.secret_token = '36447a2632340a7d45eca64b151079fa2c8e0670c8cf3f9c20c574681009eecfd7464470d1f6ff398aa04148d8909f65121910ac8c7a35604da882721eb06805'
